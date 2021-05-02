0c75ce567ed85203d2f2343cf8e93cbbd117dc65
Avishek-kr <ak251997@gmail.com>
2021-05-02 12:53:56 +0530

    running json

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const http= require('http');
 
-const server=http.createServer(()=>{
-    console.log("Hi I am Hear You")
+const server=http.createServer((request, response)=>{
+    // response.setHeader('Content-Type', 'text/html' )
+    console.log('method', request.method);
+    console.log('url', request.url);
+    const user={
+        name:"avishek",
+        hobby: "gaming"
+    }
+    response.setHeader('Content-Type', 'application/json' )
+    // response.end('<h1>HElllllllloooooooooo</h1>')   
+    response.end(JSON.stringify(user))  
+
+     
 })
 
 server.listen(3000);
