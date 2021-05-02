d627f2a546eec80e4768cdd56ae24b8d3c97713d
Avishek-kr <ak251997@gmail.com>
2021-05-02 15:48:21 +0530

    parser

--- exserver.js
+++ exserver.js
@@ -1,14 +1,27 @@
 const express= require('express');
+const bodyParser = require('body-parser')
 
-const app= express();
 
-app.use((req,res,next)=>{
-    console.log("<h1>Hellllllooooo</h1>");
-    next()
-})
-
+const app= express(); 
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+// app.use((req,res,next)=>{
+//     console.log("<h1>Hellllllooooo</h1>");
+//     next()
+// })
 app.get('/', (req,res)=>{
-    res.send("testest")
+    res.send("getting root")
 })
+app.get('/profile', (req,res)=>{
+    res.send("getting profile")
+})
+app.post('/profile', (req,res)=>{
+    console.log(req.body)
+    const user={
+        name: 'Sally',
+        hobby: 'soccer'
+    }
+    res.send("success");
+});
 
 app.listen(3000);
