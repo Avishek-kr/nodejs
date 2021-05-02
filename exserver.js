9f7c08f26fb8aa3df06ab9071b975115687be206
Avishek-kr <ak251997@gmail.com>
2021-05-02 13:35:43 +0530

    express middleware

--- exserver.js
+++ exserver.js
@@ -1,8 +1,14 @@
 const express= require('express');
 
 const app= express();
+
+app.use((req,res,next)=>{
+    console.log("<h1>Hellllllooooo</h1>");
+    next()
+})
+
 app.get('/', (req,res)=>{
-    res.send("helllllllloooo")
+    res.send("testest")
 })
 
 app.listen(3000);
